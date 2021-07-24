320442f962065bfd50db688042a8104150be5d6a
Nicolas Diaz <Nicolas.Diaz@teaminternational.com>
2021-07-23 21:13:07 -0500

    Last update for controller

--- Proyecto_Factura_V3/Proyecto_Factura_V3/Controllers/ProductsController.cs
+++ Proyecto_Factura_V3/Proyecto_Factura_V3/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Proyecto_Factura_V3.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,29 +12,67 @@ namespace Proyecto_Factura_V3.Controllers
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<ProductsController> _logger;
 
-        public ProductsController(ILogger<ProductsController> logger)
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService, ILogger<ProductsController> logger)
         {
+            _productService = productService;
+            
+
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("{id}")]
+        public ProductViewModel Get(int id) //Deberia ser (int? id) ? Acepto null y lidio con eso
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var product = _productService.GetProduct(id);
+            var viewProduct = new ProductViewModel
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                ProductId = product.ProductId
+            };
+            return viewProduct;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Potection against bots
+        public IActionResult Post([FromBody] ProductRequestPost request)
+        {
+            _productService.AddProduct(new Product
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Manufacturer = request.Manufacturer,
+                Price = request.Price,
+                DateOfEntry = DateTime.Now,
+                BranchId = request.BranchId
+            });
+            return Ok();
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken] //Potection against bots
+        public IActionResult Put([FromBody] ProductRequestPut request)
+        {
+            var product = _productService.GetProduct(request.ProductId);
+            product.Name = request.Name;
+            product.Description = request.Description;
+            product.Manufacturer = request.Manufacturer;
+            product.Price = request.Price;
+            product.BranchId = request.BranchId;
+            _productService.UpdateProduct(product);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) //Deberia ser (int? id) ? Acepto null y lidio con eso
+        {
+            _productService.DeleteProduct(id);
+            return Ok();
         }
     }
 }
